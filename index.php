61c062add95a89252fdca591162a7d31f28c4a71
kenpeter <figo2476@hotmail.com>
2015-05-13 15:11:50 +1000

    Track file download with Google Analytics

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-11063308-2']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="track_download.js"></script>
+</head>
+
+<body>
+	<a href="test.pdf" target="_blank">test.pdf</a>
+	
+	<!--<a onclick="_gaq.push(['_trackEvent','Download','PDF',this.href]);" href="manual_test.pdf" target="_blank">manually test</a>-->
+</body>
+
+</html>
+
+
+
---
+++ index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="track_download.js"></script>	
+</head>
+
+<body>
+	<a href="test.pdf">yo.pdf</a>
+</body>
+
+</html>
+
+
+
